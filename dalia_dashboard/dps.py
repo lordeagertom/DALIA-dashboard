207baf724e708580f7b3bc3ce15b85c40eaa3cac
Eager Tom <thomas.eldridge@meteoblue.com>
2025-04-04 12:19:14 +0200

    Remove debug print statements

--- dalia_dashboard/dps.py
+++ dalia_dashboard/dps.py
@@ -14,12 +14,10 @@ class DPS:
     
     def read_data(self):
         data_dir = "data"
-        print(data_dir)
         files = os.listdir(data_dir)
 
         pattern = f"DPS{self.number}"
         for file in files:
-            print(file)
             if re.search(pattern, file):
                 break
         else:
